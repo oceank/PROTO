0a28b275eeea5d5c9154c41d1f6e76a8a36f3339
Jianhai Su <suj@email.sc.edu>
2024-11-08 12:00:50 -0500

    save and load the best offline-optimized model

--- train_finetune.py
+++ train_finetune.py
@@ -4,13 +4,16 @@ from typing import Tuple
 import cloudpickle as pickle
 
 import datetime
+import json
 import time
 import gym
 import numpy as np
 import sys
 import tqdm
+import absl
 from absl import app, flags
 from ml_collections import config_flags
+from ml_collections.config_dict.config_dict import ConfigDict
 from tensorboardX import SummaryWriter
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
@@ -22,10 +25,24 @@ from evaluation import evaluate
 from learner import Learner
 import wandb
 
+from functools import singledispatch
+
+@singledispatch
+def to_serializable(val):
+    """Used by default."""
+    return str(val)
+
+
+@to_serializable.register(np.float32)
+def ts_float32(val):
+    """Used if *val* is an instance of numpy.float32."""
+    return np.float64(val)
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string('env_name', 'halfcheetah-medium-replay-v2', 'Environment name.')
 flags.DEFINE_string('save_dir', './tmp/', 'Tensorboard logging dir.')
+flags.DEFINE_string('offline_dataset_fp', None, 'Path to offline dataset.')
 flags.DEFINE_string('off_policy_algo', 'sac', 'sac or td3')
 flags.DEFINE_integer('seed', 42, 'Random seed.')
 flags.DEFINE_integer('eval_episodes', 10,
@@ -38,6 +55,9 @@ flags.DEFINE_integer('max_steps', int(1e4), 'Number of training steps.')
 #int(2e5)
 flags.DEFINE_integer('num_pretraining_steps', int(5e3),
                      'Number of pretraining steps.')
+flags.DEFINE_integer('offline_validation_budget', int(5e4), 'Number of offline validation steps.')
+# 20, 5
+flags.DEFINE_integer('eval_episodes_per_evaluation', 10, 'Number of episodes used for evaluation.')
 flags.DEFINE_float('temp', 2, 'Loss temperature')
 flags.DEFINE_float('tau_actor', 0.005, 'actor moving average')
 flags.DEFINE_float('min_temp_online', 0., 'min online temp')
@@ -46,6 +66,7 @@ flags.DEFINE_boolean('ablation', False, 'For experiments management')
 flags.DEFINE_boolean('bc_pretrain', False, 'reward-free pretraining')
 flags.DEFINE_boolean('double', True, 'Use double q-learning when offline pretrain')
 flags.DEFINE_boolean('double_online', True, 'Use double q-learning when online finetune')
+flags.DEFINE_boolean('offline_dataset_use_buffer', True, 'Use replay buffer as the offline dataset')
 flags.DEFINE_integer('replay_buffer_size', None,
                      'Replay buffer size (=max_steps if unspecified).')
 flags.DEFINE_integer('init_dataset_size', None,
@@ -177,6 +198,21 @@ def symmetric_sample(replay_buffer, replay_buffer_online, batch_size):
                     masks=np.concatenate([replay_buffer.masks[indx_off], replay_buffer_online.masks[indx_on]], axis=0),
                     next_observations=np.concatenate([replay_buffer.next_observations[indx_off], replay_buffer_online.next_observations[indx_on]], axis=0))
 
+max_steps_per_episode_dict = {
+    "kitchen-complete-v0": 280,
+    "kitchen-mixed-v0": 280,
+    "kitchen-new-v0": 280,
+    "pen-expert-v0": 100,
+    "door-expert-v0": 200,
+    "hammer-expert-v0": 200,
+    "Ant-v2": 1000,
+    "Hopper-v2": 1000,
+    "Walker2d-v2": 1000,
+    "maze2d-umaze-v1": 300,
+    "maze2d-medium-v1": 600,
+    "maze2d-large-v1": 800,
+}
+
 def main(_):
     if 'halfcheetah' in FLAGS.env_name:
         FLAGS.config.layernorm = False
@@ -184,13 +220,15 @@ def main(_):
     symmetric = FLAGS.symmetric
     np.random.seed(FLAGS.seed)
     
-    wandb.init(project='LEGO'+'_paper',
+    offline_dataset_tag = "UseBuffer" if FLAGS.offline_dataset_use_buffer else "UseNewData"
+    
+    wandb.init(project='Proto',
                sync_tensorboard=True, reinit=True,  settings=wandb.Settings(_disable_stats=True))
     wandb.config.update(flags.FLAGS)
-    wandb.run.name = f"{FLAGS.env_name}_{FLAGS.temp}"
+    wandb.run.name = f"{FLAGS.env_name}_{offline_dataset_tag}_{FLAGS.seed}_{FLAGS.temp}"
 
     ts_str = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H-%M-%S")
-    save_dir = os.path.join(FLAGS.save_dir, ts_str)
+    save_dir = os.path.join(FLAGS.save_dir, f"{wandb.run.name}_{ts_str})
 
     hparam_str_dict = dict(seed=FLAGS.seed, env=FLAGS.env_name)
     hparam_str = ','.join([
@@ -200,6 +238,44 @@ def main(_):
 
     summary_writer = SummaryWriter(os.path.join(save_dir, 'tb', hparam_str), write_to_disk=True)
     os.makedirs(save_dir, exist_ok=True)
+
+    # save experiment configuration to file
+    flags_dict = absl.flags.FLAGS.flags_by_module_dict()
+    flags_dict = {k: {v.name: v.value for v in vs} for k, vs in flags_dict.items()}
+    expr_config_filepath = f"{save_dir}/expr_config.json"
+    expr_config_dict = {k:(v.to_dict() if isinstance(v, ConfigDict) else v) for k, v in flags_dict.items()}
+    with open(expr_config_filepath, "w") as f:
+        json.dump(expr_config_dict, f, indent=4, default=to_serializable)
+
+    # load offline dataset
+    offline_dataset, _ = load_dataset_h5py(FLAGS.offline_dataset_fp)
+
+    # offline RL validation setup: similar to the setup for online RL validation
+    offline_eval_episodes = int(FLAGS.offline_validation_budget / max_steps_per_episode_dict[FLAGS.env_name])
+    offline_evals = offline_eval_episodes//FLAGS.eval_episodes_per_evaluation
+    offline_eval_epochs = list(
+        range(FLAGS.num_pretraining_steps, 0, -1*(FLAGS.num_pretraining_steps//(offline_evals-1)))
+        )
+    if (len(offline_eval_epochs) + 1) == offline_evals:
+        # the agent at the step 0 definitely has a randomly initialized policy. So, do not evaluate here
+        offline_eval_epochs.append(offline_eval_epochs[-1]//2) # the step at the middle of step 0 and the the currently earliest evaluation step
+
+    # help save the best offline agent and its performance
+    offline_eval_filepath = f"{save_dir}/eval_ave_episode_return_offline{offline_dataset_tag}.txt"
+    with open(offline_eval_filepath, "w") as f:
+        f.write(f"Experiment Time\tEpoch\tReturn\n")
+    offline_eval_file = open(offline_eval_filepath, "a")
+    best_offline_agent_epoch = -1
+    best_offline_agent_perf = -np.inf
+    best_offline_agent_filename = f"best_offline_agent_{offline_dataset_tag}.pkl"
+    best_offline_agent_filepath = f"{save_dir}/{best_offline_agent_filename}"
+
+    # help log the evaluation of the fine-tuning process
+    ft_eval_filepath = f"{save_dir}/eval_ave_episode_return_ft.txt"
+    with open(ft_eval_filepath, "w") as f:
+        f.write(f"Experiment Time\tEpoch\tReturn\n")
+    ft_eval_file = open(ft_eval_filepath, "a")
+
 
     env, env_eval, dataset, normalize_factor = make_env_and_dataset(FLAGS.env_name, FLAGS.seed, FLAGS.load_dataset_from_path)
 
@@ -295,7 +371,28 @@ def main(_):
             update_info = agent.update(batch, offline=True)  # offline
         elif i == 0:
             update_info = agent.update(batch, offline=True)  # offline
-            agent.offline2online() # offline2online transfer
+            offline = True
+            eval_stats = evaluate(agent, env_eval, FLAGS.eval_episodes_per_evaluation, offline)
+            expr_now = datetime.now()
+            expr_time_now_str = expr_now.strftime("%Y%m%d-%H%M%S")
+            offline_eval_file.write(f"{expr_time_now_str}\t{epoch}\t{eval_stats['return']}\n")
+            offline_eval_file.flush()
+
+            if best_offline_agent_perf < eval_stats['return']:
+                best_offline_agent_epoch = i
+                best_offline_agent_perf = eval_stats['return']
+                save_data = {'agent': agent, 'epoch': i, 'return': eval_stats['return']}
+                save_pickle(save_data, best_offline_agent_filename)
+            offline_eval_file.write(f"**Best Policy**\t{best_offline_agent_epoch}\t{best_offline_agent_perf}\n")
+            offline_eval_file.flush()
+            offline_eval_file.close()
+            # since the best pooling is used in the offline validation, the best offline-optimized agent is used as the initial agent of the fine-tuning process
+            best_offline_agent = load_pickle(best_offline_agent_filepath)
+            if "agent" in best_offline_agent:
+                agent = best_offline_agent['agent']
+
+            # offline2online transfer
+            agent.offline2online()
             # Free the offline replay buffer, use online buffer to better boost the performance
             if not symmetric:
                 replay_buffer = ReplayBuffer(env.observation_space, action_dim,
@@ -325,9 +422,10 @@ def main(_):
                     summary_writer.add_histogram(f'training/{k}', v, i, max_bins=512)
             summary_writer.flush()
 
-        if i % FLAGS.eval_interval == 0:
+        if (i<0 and i in offline_eval_epochs) or (i % FLAGS.eval_interval == 0):
             offline = True if i < 1 else False
-            eval_stats = evaluate(agent, env_eval, FLAGS.eval_episodes, offline)
+            eval_episodes = FLAGS.eval_episodes_per_evaluation if offline else FLAGS.eval_episodes
+            eval_stats = evaluate(agent, env_eval, eval_episodes, offline)
 
             for k, v in eval_stats.items():
                 summary_writer.add_scalar(f'evaluation/average_{k}s', v, i)
@@ -358,6 +456,21 @@ def main(_):
             np.savetxt(os.path.join(FLAGS.save_dir, f'{FLAGS.seed}.txt'),
                        eval_returns,
                        fmt=['%d', '%.1f'])
+            
+            if offline:
+                # logging the offline evaluation results
+                expr_now = datetime.now()
+                expr_time_now_str = expr_now.strftime("%Y%m%d-%H%M%S")
+                offline_eval_file.write(f"{expr_time_now_str}\t{epoch}\t{eval_stats['return']}\n")
+                offline_eval_file.flush()
+
+                if best_offline_agent_perf < eval_stats['return']:
+                    best_offline_agent_epoch = i
+                    best_offline_agent_perf = eval_stats['return']
+                    save_data = {'agent': agent, 'epoch': i, 'return': eval_stats['return']}
+                    save_pickle(save_data, best_offline_agent_filename)
+
+
 
     wandb.finish()
     sys.exit(0)
